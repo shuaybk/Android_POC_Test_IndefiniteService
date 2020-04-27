91f47542801a3c7e76b534c6c26754d805bc3795
Shuayb Khan <shuaybk@gmail.com>
2020-04-27 16:11:20 -0400

    Add basic UI for main Activity

--- app/src/main/java/com/shuayb/capstone/android/indefiniteservicetest/MainActivity.java
+++ app/src/main/java/com/shuayb/capstone/android/indefiniteservicetest/MainActivity.java
@@ -3,12 +3,27 @@ package com.shuayb.capstone.android.indefiniteservicetest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerText = (TextView)findViewById(R.id.timerText);
+    }
+
+    public void onStartButtonClick(View v) {
+
+    }
+
+    public void onStopButtonClick(View v) {
+
     }
 }
